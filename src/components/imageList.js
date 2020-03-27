0505ccc60379f75fb7134c6deba45474e02b69b2
ricardopresto <ricardopresto5@gmail.com>
2020-03-27 14:29:02 +0100

    added new images

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,31 +1,89 @@
 const imageArray = [
   [
-    "ivy_edit1",
-    "ivy_edit2",
-    "ivy_edit3",
-    "ivy_edit4",
-    "ivy_edit5",
-    "ivy_edit6",
-    "ivy_edit7",
-    "ivy_edit8"
+    "launchy3",
+    "dung1",
+    "ivy1",
+    "olympic2",
+    "scafellmoon",
+    "launchy2",
+    "runcorn",
+    "ferry1",
+    "whitstable",
+    "dung2",
+    "launchy1",
+    "olympic1",
+    "underbridge",
+    "dung3",
+    "ferry2"
   ],
-  ["ff_edit1", "ff_edit2", "ff_edit3", "ff_edit4"]
+  [
+    "battersea",
+    "eastsidegallery",
+    "peony",
+    "kreuzberg",
+    "teufelsberg1",
+    "wasdale2",
+    "canterbury1",
+    "erasmusbrug",
+    "nieuwegracht",
+    "teufelsberg2",
+    "wasdale3",
+    "canterbury2",
+    "frankfurt",
+    "olympic",
+    "teufelsberg3",
+    "dom",
+    "hetacademiegebouw",
+    "orange",
+    "duddon1",
+    "kale",
+    "wasdale1"
+  ]
 ];
 
-const nameArray = ["Ivy", "Fiddler's Ferry"];
+const nameArray = ["Black & White", "Colour"];
 
 const titleArray = [
   [
-    "ivy_edit1",
-    "ivy_edit2",
-    "ivy_edit3",
-    "ivy_edit4",
-    "ivy_edit5",
-    "ivy_edit6",
-    "ivy_edit7",
-    "ivy_edit8"
+    "Launchy Gill, Thirlmere",
+    "Dungeness, Kent",
+    "Ivy",
+    "Olympic Stadium, Berlin",
+    "Moon over Scafell, Wasdale",
+    "Launchy Gill, Thirlmere",
+    "Runcorn Bridge, Halton",
+    "Fiddler's Ferry power station, Halton",
+    "Whitstable, Kent",
+    "Lifeboat Station, Dungeness, Kent",
+    "Launchy Gill, Thirlmere",
+    "Olympic Stadium, Berlin",
+    "Bridge over the Spree, Berlin",
+    "Derek Jarman's house, Dungeness, Kent",
+    "Fiddler's Ferry power station, Halton"
   ],
-  ["ff_edit1", "ff_edit2", "ff_edit3", "ff_edit4"]
+  [
+    "Battersea power station, London",
+    "Street art, East Side Gallery, Berlin",
+    "Peony",
+    "Street art, Kreuzberg, Berlin",
+    "Teufelsberg, Berlin",
+    "Wasdale, Cumbria",
+    "Canterbury Cathedral, Kent",
+    "Erasmusbrug, Rotterdam",
+    "Nieuwegracht, Utrecht",
+    "Teufelsberg, Berlin",
+    "Wasdale, Cumbria",
+    "Canterbury Cathedral, Kent",
+    "Street art, Frankfurt",
+    "Olympic Stadium, Berlin",
+    "Teufelsberg, Berlin",
+    "Cathedral, Berlin",
+    "Het Academiegebouw, Utrecht",
+    "Orange",
+    "Duddon Valley, Cumbria",
+    "Kale",
+    "Wasdale, Cumbria"
+  ]
 ];
 
 export { imageArray, nameArray, titleArray };
